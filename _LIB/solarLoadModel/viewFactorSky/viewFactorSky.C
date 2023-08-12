SustainableUrbanSystemsLab/urbanMicroClimateFoam-BlueCFD

commit 284d9a3d00b98337eed9a681bd5128bd61c56b10

fix in viewFactorSky: grassProperties is searched in air region - otherwise, viewFactorSky could not find grassProperties in case there is also vegetation. As a result, solver uses surface temperature instead of grass temperature for long-wave radiation

_LIB/solarLoadModel/viewFactorSky/viewFactorSky.C
@@ -34,6 +34,8 @@ License
 #include "interpolationTable.H"
 #include "hashedWordList.H"
 
+#include "mappedPatchBase.H"
+
 using namespace Foam::constant;
 
 // * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
@@ -448,15 +450,17 @@ void Foam::radiation::viewFactorSky::calculate()
     //////////////////////////////////////////////////////////////////////////
     //is grass model activated?
     hashedWordList grassPatches;
+    const polyMesh& airMesh =
+        mesh_.time().lookupObject<polyMesh>("air");
     IOdictionary grassProperties
     (
-		IOobject
-		(
-		    "grassProperties",
-		    mesh_.time().constant(),
-		    mesh_,
-		    IOobject::READ_IF_PRESENT,
-		    IOobject::NO_WRITE
+        IOobject
+        (
+            "grassProperties",
+            airMesh.time().constant(), 
+            airMesh,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
         )
     );
     if (grassProperties.typeHeaderOk<IOdictionary>(true))
@@ -479,7 +483,23 @@ void Foam::radiation::viewFactorSky::calculate()
         scalarField Tg_(T_.size(), -1.0);
         if (grassPatches.contains(mesh_.boundary()[patchID].name()))
         {
-            Tg_ = mesh_.boundary()[patchID].lookupPatchField<volScalarField, scalar>("Tg");
+            if(mesh_.name() == "vegetation")
+            {
+                // Get the coupling information from the mappedPatchBase
+                const mappedPatchBase& mpp =
+                    refCast<const mappedPatchBase>(mesh_.boundary()[patchID].patch());
+                const polyMesh& nbrMesh = mpp.sampleMesh();
+                const label samplePatchI = mpp.samplePolyPatch().index();
+                const fvPatch& nbrPatch =
+                    refCast<const fvMesh>(nbrMesh).boundary()[samplePatchI];
+                scalarField TgNbr = nbrPatch.lookupPatchField<volScalarField, scalar>("Tg");
+                    mpp.distribute(TgNbr);
+                Tg_ = TgNbr;
+            }
+            else
+            {
+                Tg_ = mesh_.boundary()[patchID].lookupPatchField<volScalarField, scalar>("Tg");
+            }        
         }
 
         fvPatchScalarField& qrPatch = qrBf[patchID];

commit 61f2c26efd87819524fc0be78e012a4259e681dc

fixing minor bug introduced by the changes in 284d9a3

_LIB/solarLoadModel/viewFactorSky/viewFactorSky.C
@@ -459,7 +459,7 @@ void Foam::radiation::viewFactorSky::calculate()
             "grassProperties",
             airMesh.time().constant(), 
             airMesh,
-            IOobject::MUST_READ,
+            IOobject::READ_IF_PRESENT,
             IOobject::NO_WRITE
         )
     );