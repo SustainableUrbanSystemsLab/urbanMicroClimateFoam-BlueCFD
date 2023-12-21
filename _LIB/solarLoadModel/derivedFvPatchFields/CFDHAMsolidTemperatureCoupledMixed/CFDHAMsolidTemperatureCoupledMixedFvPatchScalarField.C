SustainableUrbanSystemsLab/urbanMicroClimateFoam-BlueCFD

commit 1af2b969f25fd13b4ae7bacfb577419319f3a15b

Long-wave radiation is updated during solid solution is reimplemented. - Otherwise, it could be an issue if long exchange timesteps are used. - Reimplements the removed functionality in commit 6b847bde and corrects it. Updates radiation every 600 seconds (duration hardcoded at the moment). - Improves the functionality introduced in commit 9a3c7f72. Now it reads radiation fields only in the first internal iteration. Updated fields are read after each 600 seconds (duration hardcoded).

_LIB/solarLoadModel/derivedFvPatchFields/CFDHAMsolidTemperatureCoupledMixed/CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField.C
@@ -55,7 +55,8 @@ CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField
     qrNbrName_("undefined-qrNbr"),
     qsNbrName_("undefined-qsNbr"),
     qrNbr(0),
-    qsNbr(0)
+    qsNbr(0),
+    timeOfLastRadUpdate(-1.0)
 {
     this->refValue() = 0.0;
     this->refGrad() = 0.0;
@@ -76,7 +77,8 @@ CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField
     qrNbrName_(psf.qrNbrName_),
     qsNbrName_(psf.qsNbrName_),
     qrNbr(psf.qrNbr),
-    qsNbr(psf.qsNbr)    
+    qsNbr(psf.qsNbr),
+    timeOfLastRadUpdate(psf.timeOfLastRadUpdate)
 {}
 
 
@@ -92,7 +94,8 @@ CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField
     qrNbrName_(dict.lookupOrDefault<word>("qrNbr", "none")),
     qsNbrName_(dict.lookupOrDefault<word>("qsNbr", "none")),
     qrNbr(Zero),
-    qsNbr(Zero)    
+    qsNbr(Zero),
+    timeOfLastRadUpdate(-1.0)
 {
     if (!isA<mappedPatchBase>(this->patch().patch()))
     {
@@ -134,7 +137,8 @@ CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField
     qrNbrName_(psf.qrNbrName_),
     qsNbrName_(psf.qsNbrName_),
     qrNbr(psf.qrNbr),
-    qsNbr(psf.qsNbr) 
+    qsNbr(psf.qsNbr),
+    timeOfLastRadUpdate(psf.timeOfLastRadUpdate)
 {}
 
 
@@ -287,7 +291,15 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
     dictionary controlDict_ = time.controlDict();
     const scalar deltaT_(readScalar(controlDict_.lookup("deltaT")));
     label moduloTest = int(time.value()/deltaT_);
-    
+    bool firstIter = false;
+    if(time.value()/deltaT_ - moduloTest < SMALL)
+    {
+        if(timeOfLastRadUpdate != time.value())
+        {
+            firstIter = true;
+        }
+    }
+
     //-- Access vegetation region and populate radiation if vegetation exists,
     //otherwise use radiation from air region --//
     regionProperties rp(time);
@@ -295,7 +307,7 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
 
     if (vegNames.size()>0)
     {
-        if(time.value()/deltaT_ - moduloTest < SMALL) //update qs and qr once at the beginning
+        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
         {
             const word& vegiRegion = "vegetation";
             const scalar mppVegDistance = 0;
@@ -322,11 +334,12 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
                 qsNbr = vegiNbrPatch.lookupPatchField<volScalarField, scalar>(qsNbrName_);
                 mppVeg.distribute(qsNbr);
             }
+            timeOfLastRadUpdate = time.value();
         }
     }
     else
     {
-        if(time.value()/deltaT_ - moduloTest < SMALL) //update qs and qr once at the beginning
+        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
         {
             if (qrNbrName_ != "none")
             {
@@ -337,7 +350,8 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
             {
                 qsNbr = nbrPatch.lookupPatchField<volScalarField, scalar>(qsNbrName_);
                 mpp.distribute(qsNbr);
-            }   
+            }
+            timeOfLastRadUpdate = time.value();
         }
     }
     //////////////////////////////

_LIB/solarLoadModel/derivedFvPatchFields/CFDHAMsolidTemperatureHeatCoeff/CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField.C
@@ -59,7 +59,8 @@ CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField
     betacoeff_(),
     pv_o_(),
     qrNbr(0),
-    qsNbr(0)  
+    qsNbr(0),
+    timeOfLastRadUpdate(-1.0)
 {
     this->refValue() = 0.0;
     this->refGrad() = 0.0;
@@ -84,7 +85,8 @@ CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField
     betacoeff_(psf.betacoeff_),
     pv_o_(psf.pv_o_),
     qrNbr(psf.qrNbr),
-    qsNbr(psf.qsNbr) 
+    qsNbr(psf.qsNbr),
+    timeOfLastRadUpdate(psf.timeOfLastRadUpdate)
 {}
 
 
@@ -104,7 +106,8 @@ CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField
     betacoeff_(dict.lookupOrDefault<scalar>("betacoeff",0)),
     pv_o_(dict.lookupOrDefault<fileName>("pv_o", "none")),
     qrNbr(Zero),
-    qsNbr(Zero) 
+    qsNbr(Zero),
+    timeOfLastRadUpdate(-1.0)
 {
     if (!isA<mappedPatchBase>(this->patch().patch()))
     {
@@ -150,7 +153,8 @@ CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField
     betacoeff_(psf.betacoeff_),
     pv_o_(psf.pv_o_),
     qrNbr(psf.qrNbr),
-    qsNbr(psf.qsNbr)   
+    qsNbr(psf.qsNbr),
+    timeOfLastRadUpdate(psf.timeOfLastRadUpdate)
 {}
 
 
@@ -314,6 +318,14 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
     dictionary controlDict_ = time.controlDict();
     const scalar deltaT_(readScalar(controlDict_.lookup("deltaT")));
     label moduloTest = int(time.value()/deltaT_);    
+    bool firstIter = false;
+    if(time.value()/deltaT_ - moduloTest < SMALL)
+    {
+        if(timeOfLastRadUpdate != time.value())
+        {
+            firstIter = true;
+        }
+    }
 
     //-- Access vegetation region and populate radiation if vegetation exists,
     //otherwise use radiation from air region --//
@@ -322,8 +334,8 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
 
     if (vegNames.size()>0)
     {
-        if(time.value()/deltaT_ - moduloTest < SMALL) //update qs and qr once at the beginning
-        {    
+        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
+        {
             const word& vegiRegion = "vegetation";
             const scalar mppVegDistance = 0;
      
@@ -349,12 +361,13 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
                 qsNbr = vegiNbrPatch.lookupPatchField<volScalarField, scalar>(qsNbrName_);
                 mppVeg.distribute(qsNbr);
             }
+            timeOfLastRadUpdate = time.value();
         }
     }
     else
     {
-        if(time.value()/deltaT_ - moduloTest < SMALL) //update qs and qr once at the beginning
-        {    
+        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
+        {
             if (qrNbrName_ != "none")
             {
                 qrNbr = nbrPatch.lookupPatchField<volScalarField, scalar>(qrNbrName_);
@@ -365,6 +378,7 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
                 qsNbr = nbrPatch.lookupPatchField<volScalarField, scalar>(qsNbrName_);
                 mpp.distribute(qsNbr);
             }
+            timeOfLastRadUpdate = time.value();
         }   
     }
     //////////////////////////////

_LIB/solarLoadModel/derivedFvPatchFields/CFDHAMsolidTemperatureCoupledMixed/CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField.H
@@ -77,6 +77,8 @@ class CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField
         //veg qs
         scalarField qsNbr;                 
 
+        scalar timeOfLastRadUpdate;
+
 public:
 
     //- Runtime type information

_LIB/solarLoadModel/derivedFvPatchFields/CFDHAMsolidTemperatureHeatCoeff/CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField.H
@@ -93,6 +93,8 @@ class CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField
         //veg qs
         scalarField qsNbr;                               	                    
 
+        scalar timeOfLastRadUpdate;
+
 public:
 
     //- Runtime type information

solid/solveSolid.H
@@ -7,6 +7,24 @@
     scalar solidInternalTime = 0;
     scalar timeToOutput = pts.deltaTValue();
     bool timeStepDecrease = false;
+    
+    scalar timeAfterLastRadUpdate = 0;
+    
+    //store old T//////////
+    PtrList<volScalarField::Boundary> T_old(fluidRegions.size());
+    forAll(fluidRegions, i)
+    {
+        rhoThermo& thermo = thermoFluid[i];
+        T_old.set
+        (
+            i,
+            new volScalarField::Boundary
+            (
+                thermo.T().boundaryField()
+            )
+        );
+    }
+    ///////////////////////
 
     while ( solidInternalTime < pts.deltaTValue() )
     {         
@@ -77,6 +95,7 @@
 
             solidInternalTime += runTime.deltaT().value();
             timeToOutput = pts.deltaTValue() - solidInternalTime;
+            timeAfterLastRadUpdate += runTime.deltaT().value();
             if (timeToOutput >= 0.0)
             {
                 runTime.setTime(runTime.value()+runTime.deltaT().value(),runTime.timeIndex()+1);
@@ -84,9 +103,79 @@
             }
             #include "setSolidRegionDeltaT.H"
         }
+        
+        if (timeToOutput > 0.0 && timeAfterLastRadUpdate >= 600.0)
+        {
+            //update thermal radiation fluxes
+            forAll(fluidRegions, i)
+            {
+                Info << "Updating T boundary fields..." << endl;
+                rhoThermo& thermo = thermoFluid[i];
+                thermo.T().correctBoundaryConditions();
+            }
+            if (vegRegions.size() > 0)
+            {
+                forAll(vegRegions, i)
+                {
+                    Info << "Updating T boundary fields..." << endl;
+                    volScalarField& vegT = TVeg[i];                   
+                    vegT.correctBoundaryConditions();
+                    Info << "Updating long-wave radiation heat transfer for region: " << vegRegions[i].name() << endl;
+                    radiation::radiationModel& rad = radiation2[i];
+                    rad.correct();
+                }
+            }
+            else
+            {
+                forAll(fluidRegions, i)
+                {
+                    Info << "Updating long-wave radiation heat transfer for region: " << fluidRegions[i].name() << endl;
+                    radiation::radiationModel& rad = radiation[i];
+                    rad.correct();
+                }
+            }
+            timeAfterLastRadUpdate = 0;
+        }        
 
         Info << "timeToOutput: " << timeToOutput << endl;
     }
+
+    //restore old T////////
+    forAll(fluidRegions, i)
+    {
+        rhoThermo& thermo = thermoFluid[i];
+        
+        volScalarField::Boundary& TBf = thermo.T().boundaryFieldRef();
+        const volScalarField::Boundary& refTBf = T_old[i];
+        
+        forAll(TBf, patchi)
+        {
+            //restore patch value fields
+            forAll(TBf[patchi], facei)
+            {
+                TBf[patchi][facei] = refTBf[patchi][facei];
+            }
+            
+            //restore patch refValue fields
+            if
+            (   
+                isA<mixedFvPatchScalarField>(TBf[patchi])
+             && isA<mixedFvPatchScalarField>(refTBf[patchi])
+            )
+            {
+                refCast<mixedFvPatchScalarField>
+                (TBf[patchi]).refValue() =
+                refCast<const mixedFvPatchScalarField>
+                (refTBf[patchi]).refValue();
+            }
+        }
+    }
+    forAll(vegRegions, i)
+    {
+        volScalarField& vegT = TVeg[i];                   
+        vegT.correctBoundaryConditions();
+    }
+    ///////////////////////
 
     runTime.TimeState::operator=(pts); //restore time state
 }

urbanMicroclimateFoam.C
@@ -52,6 +52,8 @@ Description
 
 #include "vegetationModel.H"
 
+#include "mixedFvPatchFields.H"
+
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 int main(int argc, char *argv[])

commit abc0580af20037e13623799330edfba3fa667bee

fixing bug introduced by commit 1af2b969 for grass patches

_LIB/solarLoadModel/derivedFvPatchFields/CFDHAMsolidTemperatureCoupledMixed/CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField.C
@@ -296,8 +296,14 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
     {
         if(timeOfLastRadUpdate != time.value())
         {
-            firstIter = true;
+            firstIter = true; //check if first internal iteration
         }
+    }
+    bool radUpdateNow = false;
+    if ((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update rad once at the beginning and every 600 s
+    {
+        radUpdateNow = true;
+        timeOfLastRadUpdate = time.value();
     }
 
     //-- Access vegetation region and populate radiation if vegetation exists,
@@ -307,7 +313,7 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
 
     if (vegNames.size()>0)
     {
-        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
+        if(radUpdateNow) //update qs and qr once at the beginning
         {
             const word& vegiRegion = "vegetation";
             const scalar mppVegDistance = 0;
@@ -339,7 +345,7 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
     }
     else
     {
-        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
+        if(radUpdateNow) //update qs and qr once at the beginning
         {
             if (qrNbrName_ != "none")
             {
@@ -380,7 +386,7 @@ void CFDHAMsolidTemperatureCoupledMixedFvPatchScalarField::updateCoeffs()
 
             if (grassPatches.contains(nbrPatch.name()))//if patch is covered with grass
             {
-                if(time.value()/deltaT_ - moduloTest < SMALL) //update qs and qr once at the beginning
+                if(radUpdateNow) //update qs and qr once at the beginning
                 {                
                     scalarField TgNbr = nbrPatch.lookupPatchField<volScalarField, scalar>("Tg");
                         mpp.distribute(TgNbr);

_LIB/solarLoadModel/derivedFvPatchFields/CFDHAMsolidTemperatureHeatCoeff/CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField.C
@@ -323,8 +323,14 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
     {
         if(timeOfLastRadUpdate != time.value())
         {
-            firstIter = true;
+            firstIter = true; //check if first internal iteration
         }
+    }
+    bool radUpdateNow = false;
+    if ((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update rad once at the beginning and every 600 s
+    {
+        radUpdateNow = true;
+        timeOfLastRadUpdate = time.value();
     }
 
     //-- Access vegetation region and populate radiation if vegetation exists,
@@ -334,7 +340,7 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
 
     if (vegNames.size()>0)
     {
-        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
+        if(radUpdateNow)
         {
             const word& vegiRegion = "vegetation";
             const scalar mppVegDistance = 0;
@@ -361,12 +367,11 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
                 qsNbr = vegiNbrPatch.lookupPatchField<volScalarField, scalar>(qsNbrName_);
                 mppVeg.distribute(qsNbr);
             }
-            timeOfLastRadUpdate = time.value();
         }
     }
     else
     {
-        if((firstIter) or (time.value() - timeOfLastRadUpdate >= 600.0)) //update qs and qr once at the beginning
+        if(radUpdateNow)
         {
             if (qrNbrName_ != "none")
             {
@@ -378,7 +383,6 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
                 qsNbr = nbrPatch.lookupPatchField<volScalarField, scalar>(qsNbrName_);
                 mpp.distribute(qsNbr);
             }
-            timeOfLastRadUpdate = time.value();
         }   
     }
     //////////////////////////////
@@ -407,8 +411,8 @@ void CFDHAMsolidTemperatureTransferCoeffFvPatchScalarField::updateCoeffs()
 
             if (grassPatches.contains(nbrPatch.name()))//if patch is covered with grass
             {
-                if(time.value()/deltaT_ - moduloTest < SMALL) //update qs and qr once at the beginning
-                {                    
+                if(radUpdateNow)
+                {
                     scalarField TgNbr = nbrPatch.lookupPatchField<volScalarField, scalar>("Tg");
                         mpp.distribute(TgNbr);
     